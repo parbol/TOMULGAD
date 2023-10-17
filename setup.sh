83320f64fbf3fa169633647b1c424d849497ade4
parbol <pablom@cern.ch>
2023-10-17 18:57:08 +0200

    Tested

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-export G4INSTALLDIR=/home/pablo/geant4-v11.1.2-install
-export G4WORKDIR=/home/pablo/Documentos/TOMULGAD/
-export JSONCPPDIR=/home/pablo/jsoncpp/
+if [ $HOSTNAME == "caronte" ]; then
 
-source $G4INSTALLDIR/bin/geant4.sh
+    echo "Setting up environment in Caronte"
+    export G4INSTALLDIR=/home/pablo/Documentos/TOMULGAD/geant4-v11.1.2-install
+    export G4WORKDIR=/home/pablo/Documentos/TOMULGAD/TOMULGAD/
+    export JSONCPPDIR=/home/pablo/Documentos/TOMULGAD/jsoncpp/
+    source $G4INSTALLDIR/bin/geant4.sh
+fi
+
+if [ $HOSTNAME == "euler" ]; then
+
+    echo "Setting up environment in Euler"
+    export G4INSTALLDIR=/home/pablom/geant4-v11.1.2-install
+    export G4WORKDIR=/home/pablom/Documentos/TOMULGAD/
+    export JSONCPPDIR=/home/pablom/jsoncpp/
+    source $G4INSTALLDIR/bin/geant4.sh
+fi
 
